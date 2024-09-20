7304c08be8b34f16cbe662d841f3609b862254ad
qwill <k.q.willemen@gmail.com>
2024-09-20 22:44:37 +0200

    Create Gui Buttons

--- Config_Hyper-V_TCR_GUI.ps1
+++ Config_Hyper-V_TCR_GUI.ps1
@@ -6,7 +6,7 @@ Add-Type -AssemblyName System.Drawing
 function Get-HyperVStatus {
     $feature = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
     if ($feature.State -eq "Enabled") {
-        return "Enabled"
+        return "Disabled"
     } else {
         return "Disabled"
     }
@@ -18,11 +18,45 @@ function Get-WindowsVersion {
     return $WinVer
 }
 
+#Function to config Hyper-V
+function set-HyperV {
+    #Test if folders exist otherwise Create folders
+    If (-not (Test-Path -Path "$env:SystemDrive\hv")){
+        New-Item -Name HV -Path $env:SystemDrive -ItemType Directory
+    }
+    
+    $HyperV_DirList = @(
+        "VM",
+        "ISO",
+        "Base"
+    )
+    
+    foreach ($Dir in $HyperV_DirList) {
+        If (-not(Test-path -Path "$env:SystemDrive\hv\$Dir")){
+            New-Item -Name $Dir -Path "$env:SystemDrive\HV" -ItemType Directory
+        }
+    }
+    
+    #Set Default Virtual Machine path on machine.
+    Set-VMHost -VirtualMachinePath "$env:SystemDrive\HV\VM" -VirtualHardDiskPath "$env:SystemDrive\HV\VM"    
+    #Create Internal Switch
+    New-VMSwitch -Name InternalSwitch -SwitchType Internal
+    #Create Private Switch
+    New-VMSwitch -Name PrivateSwitch -SwitchType Private
+}
+
 #Create the form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Config Hyper-V"
 $form.Size = New-Object System.Drawing.Size(400, 200)
 $form.StartPosition = "CenterScreen"
+
+#create Icon
+#$base64Icon = ""
+#$iconBytes = [Convert]::FromBase64String($base64Icon)
+#$memoryStream = New-Object System.IO.MemoryStream(,$iconBytes)
+#$icon = [System.Drawing.Icon]::FromHandle(([System.Drawing.Bitmap]::FromStream($memoryStream)).GetHicon())
+#$form.Icon = $icon
 
 #label for Windows version
 $WinVerLabel = New-Object System.Windows.Forms.Label
@@ -54,10 +88,56 @@ $hypervTextBox.ReadOnly = $true
 $hypervTextBox.Text = Get-HyperVStatus
 $form.Controls.Add($hypervTextBox)
 
+#Create a hidden Help button, appears if Windows Verion is incorrect
+$enableHelpButton = New-Object System.Windows.Forms.Button
+$enableHelpButton.Text = "OS Help"
+$enableHelpButton.Location = New-Object System.Drawing.Point(150, 100)
+$enableHelpButton.Size = New-Object System.Drawing.Size(120, 30)
+$enableHelpButton.Visible = $false  # Set to hidden by default
+$form.Controls.Add($enableHelpButton)
+
+$Winver = Get-WindowsVersion
+if (-not($Winver -like "*Pro*" -or $Winver -like "*Education*")) {
+    $enableHelpButton.Visible = $true 
+} 
+
+#Create a hidden Config button, appears if Hyper-V is enabled
+$enableConfigButton = New-Object System.Windows.Forms.Button
+$enableConfigButton.Text = "Config Hyper-V"
+$enableConfigButton.Location = New-Object System.Drawing.Point(150, 100)
+$enableConfigButton.Size = New-Object System.Drawing.Size(120, 30)
+$enableConfigButton.Visible = $false  # Set to hidden by default
+$form.Controls.Add($enableConfigButton)
+
+#Config Button Event
+$enableConfigButton.Add_Click({
+})
+
+#Create a hidden Enable button, appears if Hyper-V is enabled
+$HVEnableButton = New-Object System.Windows.Forms.Button
+$HVEnableButton.Text = "Enable Hyper-V"
+$HVEnableButton.Location = New-Object System.Drawing.Point(150, 100)
+$HVEnableButton.Size = New-Object System.Drawing.Size(120, 30)
+$HVEnableButton.Visible = $false  # Set to hidden by default
+$form.Controls.Add($HVEnableButton)
+
+#Enable Button Event
+$HVEnableButton.Add_Click({
+})
+
+#Activate Correct Buttons Based on Hyper-V Detection
+$hypervStatus = Get-HyperVStatus
+Write-host $HypervStatus
+if ($hypervStatus -eq "Disabled"){
+
+}elseif ($hypervStatus -eq "Enabled") {
+    $enableConfigButton.Visible = $true
+}
+
 #exit button
 $exitButton = New-Object System.Windows.Forms.Button
 $exitButton.Text = "Exit"
-$exitButton.Location = New-Object System.Drawing.Point(150, 100)
+$exitButton.Location = New-Object System.Drawing.Point(275, 100)
 $exitButton.Size = New-Object System.Drawing.Size(75, 30)
 $form.Controls.Add($exitButton)
 
